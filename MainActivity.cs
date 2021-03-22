d0fb40364db556594ef127af3378fafd15fcc293
sangnq2498 <34269455+nguyenquangsang2042@users.noreply.github.com>
2021-03-22 21:35:15 +0700

    Initial commit

---
+++ MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+
+namespace DictionaryApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+       
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            TextView txtShowDef = FindViewById<TextView>(Resource.Id.txtDef);
+            EditText edtWordSearch = FindViewById<EditText>(Resource.Id.editTextSearch);
+
+            edtWordSearch.TextChanged += (sender, e) =>
+            {
+                txtShowDef.Text = edtWordSearch.Text;
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
